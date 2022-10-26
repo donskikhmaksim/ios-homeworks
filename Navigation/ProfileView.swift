8d6101e0a32fee9f5e21d99f18ec62d5dbd9f8de
Donskikh.Maksim <donskikh.maksim@gmail.com>
2022-10-27 01:06:32 +0400

    additional code added (there was no such item in the homework), added custom class to "Profile.storyboard" view (view controller?).

--- Navigation/ProfileView.swift
+++ Navigation/ProfileView.swift
@@ -18,4 +18,23 @@ class ProfileView: UIView {
     @IBOutlet weak var avatarImageView: UIImageView!
     
     @IBOutlet weak var nameLabel: UILabel!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setup()
+    }
+ 
+    private func setup() {
+        let view = self.loadViewFromXib()
+        self.addSubview(view)
+        view.frame = self.bounds
+    }
+    
+    private func loadViewFromXib() -> UIView {
+        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self)?.first as? UIView else {
+            return UIView()
+        }
+        
+        return view
+    }
 }
