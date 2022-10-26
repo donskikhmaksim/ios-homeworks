ae95a1c20e134f9a9c8cb001182b57480f807eb2
Donskikh.Maksim <donskikh.maksim@gmail.com>
2022-10-27 00:52:15 +0400

    "Main.storyboard" removed, created "ProfileView.xib" with 5 UI elements, Autoresizing mask configured, "ProfileView.swift" created wit UIView inheritance, @IBOoutlet's created for elements in "ProfileView.xib", photo uploaded.

---
+++ Navigation/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Максим Донских on 27.10.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var birthDateLabel: UILabel!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+}
